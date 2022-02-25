c2bb0f5278bdb4cfbd3f05e919ae4c7e131a5ce9
DinaOluf <91533917+DinaOluf@users.noreply.github.com>
2022-02-25 12:13:38 +0100

    cleaned up some functions

--- js/details.js
+++ js/details.js
@@ -4,26 +4,16 @@ const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
 const details_url = "https://mhw-db.com/monsters/" + id;
-const detailsContainer = document.querySelector(".details-container");
+
 
 async function getMonsterDetails() {
     try {
         const response = await fetch(details_url);
         const details = await response.json();
 
-        detailsContainer.innerHTML = `<div class="title-container"><h1>${details.name}</h1></div>
-                        <p>Size: ${details.type}</p>
-                        <p>Species: ${details.species}</p>
-                        <p>Description: ${details.description}</p>
-                        <ul class="location">Location(s):</ul>`
+        getHtml(details);
 
         getImageBySpecies(details);
-
-        const locationList = document.querySelector(".location");
-
-        details.locations.forEach(function(location) {
-            locationList.innerHTML += `<li>${location.name}</li>`
-         });
 
     } catch (error) {
     resultContainer.innerHTML = displayError("An error occurred when calling the API");
@@ -32,9 +22,24 @@ async function getMonsterDetails() {
 
 getMonsterDetails();
 
+function getHtml(details) {
+    const detailsContainer = document.querySelector(".details-container");
+
+    detailsContainer.innerHTML = `<div class="title-container"><h1>${details.name}</h1></div>
+    <p>Size: ${details.type}</p>
+    <p>Species: ${details.species}</p>
+    <p>Description: ${details.description}</p>
+    <ul class="location">Location(s):</ul>`
+
+    const locationList = document.querySelector(".location");
+
+    details.locations.forEach(function(location) {
+        locationList.innerHTML += `<li>${location.name}</li>`
+     });
+}
+
 function getImageBySpecies(details){
     const titleContainer = document.querySelector(".title-container");
-    console.log(titleContainer);
 
     if(details.species.toLowerCase() === "herbivore"){
         titleContainer.innerHTML = '<img src="../monster-icons/Herbivore.png" class="monster-img">' + titleContainer.innerHTML
--- js/index.js
+++ js/index.js
@@ -14,22 +14,8 @@ async function getMonsterCards() {
 
         resultContainer.innerHTML = "";
 
-        for(let i = 0; i < monsters.length; i++){
-
-            resultContainer.innerHTML += `<a href="details.html?id=${monsters[i].id}"><div class="monster-cards">
-                        <h2>${monsters[i].name}</h2>
-                        <p>Size: ${monsters[i].type}</p>
-                        <p class="species">Species: ${monsters[i].species}</p>
-                        <p class="loc">Location: ${monsters[i].locations[0].name}</p>
-                    </div></a>`
-
-            if(monsters[i].locations.length > 1) {
-                const locContainer = document.querySelectorAll(".loc");
-
-                locContainer[i].innerHTML += ` (...)`
-            }
-        }
-
+        addHtml(monsters);
+        
     } catch (error) {
     resultContainer.innerHTML = displayError("An error occurred when calling the API");
 }
@@ -37,3 +23,20 @@ async function getMonsterCards() {
 
 getMonsterCards();
 
+function addHtml(monsters) {
+    for(let i = 0; i < monsters.length; i++){
+
+        resultContainer.innerHTML += `<a href="details.html?id=${monsters[i].id}"><div class="monster-cards">
+                    <h2>${monsters[i].name}</h2>
+                    <p>Size: ${monsters[i].type}</p>
+                    <p class="species">Species: ${monsters[i].species}</p>
+                    <p class="loc">Location: ${monsters[i].locations[0].name}</p>
+                </div></a>`
+
+        if(monsters[i].locations.length > 1) {
+            const locContainer = document.querySelectorAll(".loc");
+
+            locContainer[i].innerHTML += ` (...)`
+        }
+    }
+}
